6f92e53af3adf1427c70e5ca2222cd5fe5b618fe
3Pratyush <167027820+3Pratyush@users.noreply.github.com>
2026-06-09 18:04:25 +0530

    .

--- src/main/java/listeners/TestListener.java
+++ src/main/java/listeners/TestListener.java
@@ -1,5 +1,38 @@
 package listeners;
 
-public class TestListener {
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("Execution Started");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("Execution Finished");
+    }
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("Running Test : " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("PASSED : " + result.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("FAILED : " + result.getName());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("SKIPPED : " + result.getName());
+    }
 }
